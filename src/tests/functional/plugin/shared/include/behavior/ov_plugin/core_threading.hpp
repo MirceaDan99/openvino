5d43ea2b3a7d503b00e11d29d1e9aba64a207e79
MirceaDan99 <mircea-aurelian.dan@intel.com>
2024-04-12 18:20:13 +0300

    Use `OV_ASSERT_NO_THROW` again as `std::cout` + `std::exit` would not print any exception

--- src/tests/functional/plugin/shared/include/behavior/ov_plugin/core_threading.hpp
+++ src/tests/functional/plugin/shared/include/behavior/ov_plugin/core_threading.hpp
@@ -29,15 +29,7 @@ public:
         for (auto& thread : threads) {
             thread = std::thread([&]() {
                 for (unsigned int i = 0; i < iterations; ++i) {
-                    try {
-                        func();
-                    } catch (ov::Exception e) {
-                        std::cout << "Exception occurred:" << std::endl << e.what();
-                        std::exit(EXIT_FAILURE);
-                    } catch (...) {
-                        std::cout << "Unkown exception occurred!" << std::endl;
-                        std::exit(EXIT_FAILURE);
-                    }
+                    OV_ASSERT_NO_THROW(func());
                 }
             });
         }

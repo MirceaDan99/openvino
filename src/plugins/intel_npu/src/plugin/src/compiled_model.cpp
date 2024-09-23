1405c531111e7d477a1f544518fc3142735f6236
MirceaDan99 <mircea-aurelian.dan@intel.com>
2024-09-23 17:56:51 +0300

    Add `custom allocator` for `std::vector`

--- src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
@@ -27,11 +27,10 @@ constexpr std::string_view NO_EXECUTOR_FOR_INFERENCE =
     "Can't create infer request!\n"
     "Please make sure that the device is available. Only exports can be made.";
 
-std::uint32_t hash(const intel_npu::CompiledNetwork& blob) {
+std::uint32_t hash(const std::vector<uint8_t>& blob) {
     std::uint32_t result = 1171117u;
-    for (const uint8_t* it = blob.data; it != blob.data + blob.size; ++it) {
-        result = ((result << 7) + result) + static_cast<uint32_t>(*it);
-    }
+    for (auto c : blob)
+        result = ((result << 7) + result) + static_cast<uint32_t>(c);
     return result;
 }
 
@@ -141,14 +140,14 @@ std::shared_ptr<ov::ISyncInferRequest> CompiledModel::create_sync_infer_request(
 void CompiledModel::export_model(std::ostream& stream) const {
     _logger.debug("CompiledModel::export_model");
     const auto blob = _compiler->getCompiledNetwork(*_networkPtr);
-    stream.write(reinterpret_cast<const char*>(blob.data), blob.size);
+    stream.write(reinterpret_cast<const char*>(blob.data()), blob.size());
 
     if (!stream) {
         _logger.error("Write blob to stream failed. Blob is broken!");
     } else {
         if (_logger.level() >= ov::log::Level::INFO) {
             std::stringstream str;
-            str << "Blob size: " << blob.size << ", hash: " << std::hex << hash(blob);
+            str << "Blob size: " << blob.size() << ", hash: " << std::hex << hash(blob);
             _logger.info(str.str().c_str());
         }
         _logger.info("Write blob to stream successfully.");
